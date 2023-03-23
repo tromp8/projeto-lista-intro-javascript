01bb049994ca25c96ac08dca3ed2310d981aba8e
unknown <deboraluciak@hotmail.com>
2023-03-23 17:48:06 -0300

    projeto js labenu

--- exercicios.js
+++ exercicios.js
@@ -3,7 +3,8 @@
 // EXERCÍCIO 0A
 function soma(num1, num2) {
   // implemente sua lógica aqui
-  return num1 + num2
+  const soma = num1 + num2
+  return soma
 }
 
 // EXERCÍCIO 0B
@@ -11,58 +12,74 @@ function imprimeMensagem() {
   // implemente sua lógica aqui
   const mensagem = prompt('Digite uma mensagem!')
 
-  console.log(mensagem)
+  console.log (mensagem) 
 }
 
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
+  const altura = prompt ('Digite a altura:')
+  const largura = prompt ('Digite a largura:')
+  const area = altura*largura
 
+  console.log (area)
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
+  const anoAtual = prompt ('Digite o ano atual:')
+  const anoNasc = prompt ('Digite o ano em que nasceu:')
+  const idade = anoAtual-anoNasc
 
+  console.log (idade)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
+  const imc = peso / (altura*altura)
 
+  return imc
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  const nome = prompt ("Digite seu nome")
+  const idade = prompt ("Digite seu idade")
+  const email = prompt ("Digite seu email")
 
+  console.log (`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 }
+
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+  let cor1 = prompt ('Digite uma cor')
+  let cor2 = prompt ('Digite uma cor')
+  let cor3 = prompt ('Digite uma cor')
 
+  const listaCores = [cor1,cor2,cor3]
+  console.log (listaCores)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
-
+  const stringUp = string.toUpperCase ()
+  
+  return stringUp
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+  const ingressosVendidos = custo/valorIngresso
 
+  return ingressosVendidos
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-
+  
 }
 
 // EXERCÍCIO 09
